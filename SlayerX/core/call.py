29768bcefd8d5cfaaf5a078c665a479cfdd4a6d9
𝙽𝙾𝙱𝙸𝚃𝙰_𝚇𝙳 <104634763+nobitaaaxd@users.noreply.github.com>
2024-04-07 15:37:04 +0530

    Update call.py

--- SlayerX/core/call.py
+++ SlayerX/core/call.py
@@ -2,17 +2,19 @@ import asyncio
 import os
 from datetime import datetime, timedelta
 from typing import Union
+
 from pyrogram import Client
 from pyrogram.types import InlineKeyboardMarkup
 from pytgcalls import PyTgCalls, StreamType
-from pytgcalls.exceptions 
-
+from pytgcalls.exceptions import (
+    AlreadyJoinedError,
+    NoActiveGroupCall,
+    TelegramServerError,
+)
 from pytgcalls.types import Update
 from pytgcalls.types.input_stream import AudioPiped, AudioVideoPiped
 from pytgcalls.types.input_stream.quality import HighQualityAudio, MediumQualityVideo
 from pytgcalls.types.stream import StreamAudioEnded
-
-import AlreadyJoinedError,NoActiveGroupCall,TelegramServerError,
 
 import config
 from SlayerX import LOGGER, YouTube, app
